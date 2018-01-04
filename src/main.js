c23419a69d9c6ffeba11ecd8de752d3c960dbd0e
Aaron Lampros <alampros@gmail.com>
2018-01-04 09:28:54 -0500

    Render icons to main artboard

--- src/main.js
+++ src/main.js
@@ -1,21 +1,80 @@
 import React from 'react'
-import { Document, Page, Text, render } from 'react-sketchapp'
-import './allIcons.js'
+import { Document, Page, Artboard, View, Text, render } from 'react-sketchapp'
+import pkg from '../package.json'
+import allIcons from './allIcons.js'
 
 const IconsPage = () => {
-  return (
-    <Document>
-      <Page name="Nothing to see here">
+  const $icons = Object.keys(allIcons).map(iconMemberName => {
+    const iconProps = { name: 'Icon' }
+    const IconFactory = allIcons[iconMemberName]
+    // Exports are factory classes if ICONS_SKETCH_MAKE_SYMBOLS is true, but elements if not
+    const $icon = React.isValidElement(IconFactory)
+      ? React.cloneElement(IconFactory, iconProps)
+      : <IconFactory {...iconProps} />
+    return (
+      <View
+        style={{
+          margin: 20,
+          flexDirection: 'column',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          width: 130,
+        }}
+        name={iconMemberName}
+        key={iconMemberName}
+      >
+        {$icon}
         <Text
           style={{
-            fontFamily: 'Helvetica',
-            fontSize: 32,
-            fontWeight: 'bold',
-            fontColor: '#000',
+            textAlign: 'center',
+            color: '#ccc',
+            fontSize: 12,
+            marginTop: 15,
           }}
         >
-          See the symbols page
+          {iconMemberName}
         </Text>
+      </View>
+    )
+  })
+  return (
+    <Document>
+      <Page name={pkg.name}>
+        <View name="Header" style={{ left: 20, top: 20 }}>
+          <Text
+            style={{
+              fontFamily: 'Helvetica',
+              fontSize: 32,
+              fontWeight: 'bold',
+              color: '#000',
+            }}
+          >
+            ↖︎ See the symbols page
+          </Text>
+          <Text
+            style={{
+              fontFamily: 'Consolas',
+              fontSize: 56,
+              fontWeight: 'bold',
+              marginTop: 30,
+              color: '#999',
+            }}
+          >
+            {pkg.name}
+          </Text>
+        </View>
+        <Artboard
+          name="Icons"
+          style={{
+            flexDirection: 'row',
+            flexWrap: 'wrap',
+            width: 900,
+            left: 20,
+            marginTop: 70, // 30 + top + top
+          }}
+        >
+          {$icons}
+        </Artboard>
       </Page>
     </Document>
   )
